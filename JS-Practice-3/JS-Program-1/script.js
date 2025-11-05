4efe52e46928f30834e63088a4ec3872db5ea644
Bilal Ahmed <bilalahmedzvn@gmail.com>
2025-11-05 01:33:22 -0800

    Add-JS-Practice-3-Program-1

---
+++ JS-Practice-3/JS-Program-1/script.js
@@ -0,0 +1,10 @@
+let a = "Bilal";
+let b = 500;
+let c = true;
+let d = null;
+let e;
+let f = 12238488485958938465896348530583470347747447883n;
+let g = Symbol();
+
+console.log(a , b , c , d , e , f , g);
+console.log(typeof a , typeof b , typeof c , typeof d , typeof e , typeof f , typeof g);
