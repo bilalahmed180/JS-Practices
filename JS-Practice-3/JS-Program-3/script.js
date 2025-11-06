f42f090cfc381d209eede64ae6a9b1a267de9a10
Bilal Ahmed <bilalahmedzvn@gmail.com>
2025-11-06 00:41:23 -0800

    complete Program-3

---
+++ JS-Practice-3/JS-Program-3/script.js
@@ -0,0 +1,21 @@
+const name = prompt("What's Your Name");
+
+
+const age = prompt("What's Your Age");
+let ageconvert = Number();
+
+const student = prompt("You Are Student");
+let studentconvert = Boolean(); 
+
+const fav_num = prompt("What's Your Favorite Number");
+let fav_numconvert = Number();
+
+
+const nick_name = prompt("What's Your Nick Name");
+
+
+console.log("Name" + ":" + name + "-" + typeof name);
+console.log("Age" + ":" + age + "-" + typeof ageconvert);
+console.log("Is Student" + ":" + student + "-" + typeof studentconvert);
+console.log("Favorite Number" + ":" + fav_num + "-" + typeof fav_numconvert);
+console.log("Nickname" + ":" + nick_name + "-" + typeof nick_name);
