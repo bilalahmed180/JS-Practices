fdcde0fcddec329dec40f419c829eaa3513d523b
Bilal Ahmed <bilalahmedzvn@gmail.com>
2025-11-07 16:57:25 +0500

    fix bug

--- JS-Practice-4/JS-Program-1/index.js
+++ JS-Practice-4/JS-Program-1/index.js
@@ -5,7 +5,7 @@ let secondnum = prompt("Enter Your First Second Number");
 secondnum = Number(secondnum)
 
 
-let operator = prompt("Which Methode use + , - , * , /");
+let operator = prompt("Which Methode use + , - , * , / , %");
 
 let result = (operator = "+" ? firstnum + secondnum : error , operator = "-" ? firstnum - secondnum : error , operator = "*" ? firstnum * secondnum : error , operator = "/" ? firstnum / secondnum : error , operator = "%" ? firstnum % secondnum : error);
 
