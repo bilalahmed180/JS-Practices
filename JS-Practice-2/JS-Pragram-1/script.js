048b0b1d8407ce28fd9a290a4a2330c6976ca5d9
Bilal Ahmed <bilalahmedzvn@gmail.com>
2025-11-10 09:56:57 +0500

    Sovle Bugs

--- JS-Practice-2/JS-Pragram-1/script.js
+++ JS-Practice-2/JS-Pragram-1/script.js
@@ -1,10 +1,12 @@
 var name = ("Bilal")
 var city = ("Hyderabad")
 var age = ("16")
-age = ("17")
+var ageconvert = ("17")
 
 console.log(name);
 console.log(city);
 console.log(age);
+console.log(ageconvert);
+
 
 
---
+++ JS-Practice-4/JS-Program-2/script.js
@@ -0,0 +1,7 @@
+let useremail = prompt("Enter Your Email");
+let savedemail = useremail;
+
+let userpassword = prompt("Enter Your Password");
+let savedpassword = userpassword;
+
+console.log("Login Successfull :" , useremail == savedemail && userpassword == savedpassword);
